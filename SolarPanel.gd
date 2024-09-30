2a854c862ad1b786bbdc9e5b885d5f751a24901d
Sai Ethihas Chanda <127788928+SaiEthihasChanda@users.noreply.github.com>
2024-09-30 10:44:52 +0530

    can place objects and tested energy incrementor

---
+++ SolarPanel.gd
@@ -0,0 +1,18 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(sing.energy_points)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	sing.energy_points +=1
+	print(sing.energy_points)
---
+++ global.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var energy_points = 30
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("global ready")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _physics_process(delta):
+	#print(energy_points)
